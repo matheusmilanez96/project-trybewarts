1f0b7491ae385671685dbabe01eba263a8af9cba
Alexandre Castelleoni <a.castelleoni@gmail.com>
2022-11-17 17:00:57 -0300

    ex 5 e 6

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ let inputSenha = document.querySelector(".password")
 
 buttonLogin.addEventListener('click', function(event){
     event.preventDefault();
-    console.log(inputEmail.value);
-    console.log(inputSenha.value);
+    //console.log(inputEmail.value);
+    //console.log(inputSenha.value);
     if (inputEmail.value == "tryber@teste.com" && inputSenha.value == "123456"){
         alert("Olá, Tryber!")
     }
