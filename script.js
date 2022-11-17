b96b2d258a2182b2c0430fb036df4993a52d655e
Alexandre Castelleoni <a.castelleoni@gmail.com>
2022-11-17 20:45:30 -0300

    arrumando lint

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-//function
+// function
 
 const buttonLogin = document.querySelector(".login-button")
-let inputEmail = document.querySelector(".email")
-let inputSenha = document.querySelector(".password")
+const inputEmail = document.querySelector(".email")
+const inputSenha = document.querySelector(".password")
 
 
 
-buttonLogin.addEventListener('click', function(event){
+buttonLogin.addEventListener('click', function(event) {
     event.preventDefault();
-    //console.log(inputEmail.value);
-    //console.log(inputSenha.value);
+    // console.log(inputEmail.value);
+    // console.log(inputSenha.value);
     if (inputEmail.value == "tryber@teste.com" && inputSenha.value == "123456"){
         alert("Olá, Tryber!")
     }
